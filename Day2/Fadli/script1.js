352c522a59a8eb5bf36b3ff719784dc66becd6a0
Ojk <45289879+Ojak091@users.noreply.github.com>
2025-02-12 01:33:15 +0700

    add file for task Fadl

---
+++ Day2/Fadli/script1.js
@@ -0,0 +1,8 @@
+let nama = "Fadli";
+let umur = 19;
+let alamat = "Padang";
+
+//menampilkan data ke dalam element HTML
+document.getElementById("nama").textContent = "nama: " + nama;
+document.getElementById("umur").textContent = "umur: " + umur + " tahun";
+document.getElementById("alamat").textContent = "alamat: " + alamat;
--- Day2/Ojak/Scripts.js
+++ Day2/Ojak/Scripts.js
@@ -1,15 +1,15 @@
 // perbedaan var dan let
 function Data() {
   // Defenisi variable
-  var nama = "Abdul Rozaq";
+  var nama = "Abdul Rozaq"; // variable var (global Scope)
   let jabatan = "Web Developer";
   let umur = 20;
   let alamat = "Kota Padang";
   if (true) {
     // let terbatas di dalam scopenya
-    let nama = "Zeo";
+    let nama = "Zeo"; // variable let (just fo in scope) di dalam scope if
   }
-  document.getElementById("nama").textContent = "Nama: " + nama;
+  document.getElementById("nama").textContent = "Nama: " + nama; //akan memanggil variable var dengan value (Abdul Rozaq)
   document.getElementById("jabatan").textContent = "Jabatan: " + jabatan;
   document.getElementById("umur").textContent = "Umur: " + umur + " tahun";
   document.getElementById("alamat").textContent = "Alamat: " + alamat;
