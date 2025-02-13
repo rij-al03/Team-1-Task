5a52269b771179d15f0fde5b8cbea12c2fa4a307
Ojk <45289879+Ojak091@users.noreply.github.com>
2025-02-13 13:30:43 +0700

    Add ann

---
+++ Day3/Aan/lat_js2farhan.js
@@ -0,0 +1,22 @@
+//tugas no 1
+//modulus(%)
+//tidak sama dengan (!=)
+//pangkat (**)
+
+//tugas no 2
+var jam = 5;
+if(jam >= 5)console.log("anda sudah boleh pulang")
+
+//tugas no 3
+var angka=51;
+if(angka % 2 == 0)console.log("bilangan genap");
+else console.log("bilangan ganjil");
+
+//tugas no 4
+var umur = 12;
+
+if (umur >= 16) {
+    console.log("remaja");
+} else {
+    console.log("anak-anak");
+}
