9880952a60bebe718e72a106b9123f972358359c
Ojk <45289879+Ojak091@users.noreply.github.com>
2025-02-18 16:27:35 +0700

    commits for src

---
+++ belajar-vite/src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "d:/belajar-vite/src/components/navBar";
+import HomePage from "d:/belajar-vite/src/pages/HomePage";
+import Footer from "d:/belajar-vite/src/components/Footer";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
